512ee54fda34edf28a59ec363d8d4836e270464d
Jason Stathopulos <jstathopulos@gmail.com>
2016-04-18 10:44:41 -0400

    Fix Liniting In Two Way Data Binding

--- src/two-way-data-binding/two-way-data-binding.jsx
+++ src/two-way-data-binding/two-way-data-binding.jsx
@@ -1,28 +1,28 @@
 // One time theme setup (put this in an initialization file and call it once)
-import Frig from "frig"
-import FriggingBootstrap from "frigging-bootstrap"
+import Frig from 'frig'
+import FriggingBootstrap from 'frigging-bootstrap'
 Frig.defaultTheme(FriggingBootstrap)
 // Libraries needed for each component
-import React from "react"
-import ReactDOM from "react-dom"
-import {Form, Input, Submit} from "frig"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Form, Input, Submit } from 'frig'
 
 class TwoWayDataBindingExample extends React.Component {
-  displayName = "TwoWayDataBindingExample"
-  state = {account: {}}
+  displayName = 'TwoWayDataBindingExample'
+  state = { account: {} }
 
   render() {
     return (
       <div>
         <Form
           data={this.state.account}
-          onChange={(account) => this.setState({account})}
+          onChange={(account) => this.setState({ account })}
         >
           <div className="row">
-            <Input name="email"/>
-            <Input name="password"/>
-            <Input name="rememberMe" type="switch"/>
-            <Submit title="Sign In"/>
+            <Input name="email" />
+            <Input name="password" />
+            <Input name="rememberMe" type="switch" />
+            <Submit title="Sign In" />
           </div>
         </Form>
         <h3>State:</h3>
@@ -33,7 +33,7 @@ class TwoWayDataBindingExample extends React.Component {
 
 }
 
-window.addEventListener('load', function() {
-  var el = document.getElementById('two-way-data-binding-example')
-  ReactDOM.render(<TwoWayDataBindingExample/>, el)
+window.addEventListener('load', () => {
+  const el = document.getElementById('two-way-data-binding-example')
+  ReactDOM.render(<TwoWayDataBindingExample />, el)
 })
