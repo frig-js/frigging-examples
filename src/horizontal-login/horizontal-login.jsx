f7d119c2916de58fd6d7560f5f5b26c43a64d804
Jason Stathopulos <jstathopulos@gmail.com>
2016-04-18 10:49:17 -0400

    Fix Liniting In Horizontal Login

--- src/horizontal-login/horizontal-login.jsx
+++ src/horizontal-login/horizontal-login.jsx
@@ -1,19 +1,19 @@
 // One time theme setup (put this in an initialization file and call it once)
-import Frig from "frig"
-import FriggingBootstrap from "frigging-bootstrap"
+import Frig from 'frig'
+import FriggingBootstrap from 'frigging-bootstrap'
 Frig.defaultTheme(FriggingBootstrap)
-Frig.typeMapping.boolean = {component: "switch"}
+Frig.typeMapping.boolean = { component: 'switch' }
 // Libraries needed for each component
-import React from "react"
-import ReactDOM from "react-dom"
-import {Form, Input, Submit, FormErrorList} from "frig"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Form, Input, Submit, FormErrorList } from 'frig'
 
 class HorizontalLoginExample extends React.Component {
-  displayName = "HorizontalLoginExample"
+  displayName = 'HorizontalLoginExample'
 
   state = {}
 
-  columnClasses = "col-lg-4 col-lg-offset-4 col-md-6 col-md-offset-3 col-xs-12"
+  columnClasses = 'col-lg-4 col-lg-offset-4 col-md-6 col-md-offset-3 col-xs-12'
 
   render() {
     return (
@@ -27,21 +27,21 @@ class HorizontalLoginExample extends React.Component {
           >
             <div className="row">
               <h2 className="col-xs-12">Sign In</h2>
-              <FormErrorList/>
+              <FormErrorList />
               <Input
                 name="email"
                 block
                 autoFocus
-                inputHtml={{autoComplete: "off"}}
+                inputHtml={{ autoComplete: 'off' }}
               />
               <Input
                 name="password"
                 block
-                inputHtml={{autoComplete: "off"}}
+                inputHtml={{ autoComplete: 'off' }}
               />
               <Input
                 name="turbo_mode"
-                labelWidth={{xs: 6}}
+                labelWidth={{ xs: 6 }}
                 type="boolean"
               />
               <div className="col-xs-12">
@@ -60,7 +60,7 @@ class HorizontalLoginExample extends React.Component {
   }
 }
 
-window.addEventListener("load", function() {
-  let el = document.getElementById("horizontal-login")
-  ReactDOM.render(<HorizontalLoginExample/>, el)
+window.addEventListener('load', () => {
+  const el = document.getElementById('horizontal-login')
+  ReactDOM.render(<HorizontalLoginExample />, el)
 })
